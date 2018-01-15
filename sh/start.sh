0880c18eccc7c93f1d446f6af60e9dd2692c320a
Glitch (curvy-harbor) <none>
2018-01-15 21:58:47 +0000

    👏🐢 Checkpoint sh/start.sh:8/147 sh/watchers.sh:8/101

--- sh/start.sh
+++ sh/start.sh
@@ -4,17 +4,6 @@ set -e
 # json file cache:
 mkdir -p cache
 
-coffee --watch --transpile --compile . &
-nodemon --exec "bash sh/compile-jade.sh" --ext jade &
-watchify client.js -o public/client.js -v &
-nodemon --exec "bash sh/uglify.sh" --watch public/client.js
-stylus \
-  --watch \
-  --use autoprefixer-stylus \
-  --sourcemap \
-  --compress public/styles.styl \
-  --out public/styles.css \
-  &
-
+bash sh/watchers.sh &  
 
 nodemon --exec "coffee server.coffee" --watch server.coffee --watch routes.coffee
--- sh/watchers.sh
+++ sh/watchers.sh
@@ -4,7 +4,7 @@ set -e
 coffee --watch --transpile --compile . &
 nodemon --exec "bash sh/compile-jade.sh" --ext jade &
 watchify client.js -o public/client.js -v &
-nodemon --exec "bash sh/uglify.sh" --watch public/client.js
+nodemon --delay 2.5 --exec "bash sh/uglify.sh" --watch public/client.js
 stylus \
   --watch \
   --use autoprefixer-stylus \
@@ -12,5 +12,5 @@ stylus \
   --compress public/styles.styl \
   --out public/styles.css \
   &
-
+  
 wait
