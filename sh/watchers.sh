151bc7442b785177c8d8181774b9d3306919b339
Glitch (quaint-behavior) <none>
2018-01-22 22:06:22 +0000

    👆👨 Checkpoint sh/watchers.sh:8/121 public/emoji.styl:8/6 presenters/layout.coffee:8/10 templates/pages/user.jade:8/41 presenters/pages/user.coffee:8/47 presenters/projects-list.coffee:8/23 templates/projects-list.jade:8/52 templates/deleted-projects.jade:8/46

--- sh/watchers.sh
+++ sh/watchers.sh
@@ -5,7 +5,12 @@ coffee --watch --transpile --compile . &
 nodemon --exec "bash sh/compile-jade.sh" --ext jade &
 watchify client.js -o public/client.js -v &
 nodemon --delay 2.5 --exec "bash sh/uglify.sh" --watch public/client.js
-nodemon --delay 2.5 --exec "stylus --use autoprefixer-stylus --sourcemap --compress public/styles.styl --out public/styles.css" --watch public/styles.css
-
+stylus \
+  --watch \
+  --use autoprefixer-stylus \
+  --sourcemap \
+  --compress public/styles.styl \
+  --out public/styles.css \
+  &
   
 wait
