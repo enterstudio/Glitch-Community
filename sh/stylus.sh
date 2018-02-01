17c2afea8ca58541b0f42731c0fed66e35e368fa
Glitch (quaint-behavior) <none>
2018-02-01 21:57:21 +0000

    🌲📰 Checkpoint sh/watchers.sh:8/34 sh/stylus.sh:8/340 public/deleted-projects.styl:8/106

---
+++ sh/stylus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stylus tends to crash, so run it in a loop so that it'll come back to life on failure.
+
+echo "Starting stylus watcher"
+
+while true; do
+  stylus \
+    --watch \
+    --use autoprefixer-stylus \
+    --sourcemap \
+    --compress public/styles.styl \
+    --out public/styles.css
+  
+  echo "Restarting stylus watcher"
+done
--- sh/watchers.sh
+++ sh/watchers.sh
@@ -5,12 +5,6 @@ coffee --watch --transpile --compile . &
 nodemon --exec "bash sh/compile-jade.sh" --ext jade &
 watchify client.js -o public/client.js -v &
 nodemon --delay 2.5 --exec "bash sh/uglify.sh" --watch public/client.js &
-stylus \
-  --watch \
-  --use autoprefixer-stylus \
-  --sourcemap \
-  --compress public/styles.styl \
-  --out public/styles.css \
-  &
+bash sh/stylus.sh &
   
 wait
