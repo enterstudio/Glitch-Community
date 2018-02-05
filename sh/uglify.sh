ea073ba81fe6f268c4ed6545fb79bbed2b0c48d6
Glitch (community) <none>
2018-02-05 22:25:24 +0000

    🖇👙 Checkpoint sh/uglify.sh:8/1

--- sh/uglify.sh
+++ sh/uglify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
 uglifyjs public/client.js \
       --compress \
       --mangle \
       --screw-ie8 \
-      > public/client.min.js
+      > public/client.min.js.tmp
+echo "Updating public/client.min.js"
+mv public/client.min.js.tmp public/client.min.js
