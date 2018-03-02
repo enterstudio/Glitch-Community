1bf91557ea394c6f8dc4ad508138c10b9c919390
Glitch (upbeat-brochure) <none>
2018-03-02 22:23:39 +0000

    🍅💑 Checkpoint webpack.config.js:8/4 src/.eslintrc.js:8/267 watch.json:8/1

---
+++ src/.eslintrc.js
@@ -0,0 +1,29 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = exports = {
+  "extends": "eslint:recommended",
+  env: {
+      'es6': true,        // We are writing ES6 code
+      'browser': true,    // for the browser
+      'commonjs': true,   // using webpack
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+    "allowImportExportEverywhere": true
+  },
+  "rules": {
+    // Intentionally off:
+    "no-console": OFF,
+    
+    // Unintentionally off -- we should fix these, then enable them
+    // Looking for a good first PR to contribute to the site?
+    // Turn one of these on and fix it up!
+    "no-undef": OFF,
+    "no-unused-vars": OFF,
+    "no-useless-escape": OFF,
+    "no-empty": OFF,
+  }
+}
