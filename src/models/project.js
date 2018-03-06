931cc9afe9c6659df0102dd8f717ce9d33f503db
Glitch (quiver-column) <none>
2018-03-06 16:25:29 +0000

    🐾💶 Checkpoint package.json:8/13 routes.js:8/130 src/presenters/analytics.js:8/22 src/models/project.js:8/1

--- src/models/project.js
+++ src/models/project.js
@@ -8,6 +8,7 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
+/* global application */
 let Project;
 const cache = {};
 
